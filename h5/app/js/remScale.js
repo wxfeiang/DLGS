e6e6387afb1c40e9d29da7114cdbd80993859a36
wxfeiang <wxfeiang@qq.com>
2019-12-17 22:46:47 +0800

    122

--- h5/app/js/remScale.js
+++ h5/app/js/remScale.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 window.onresize = window.onload = function () {
   var deviceWidth = document.documentElement.clientWidth; /*获取设备可视区宽*/
-  if (deviceWidth > 750) deviceWidth = 750; /*最大不超过720，720为设计稿尺寸*/
+  // if (deviceWidth > 750) deviceWidth = 750; /*最大不超过720，720为设计稿尺寸*/
   document.documentElement.style.fontSize = deviceWidth / 10 + 'px';
 
 
