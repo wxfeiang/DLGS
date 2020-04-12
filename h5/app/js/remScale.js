9b17134a560e81b743ddd55a13b330735139d2cd
wxfeiang <wxfeiang@qq.com>
2020-04-12 13:40:04 +0800

    修改了sass

--- h5/app/js/remScale.js
+++ h5/app/js/remScale.js
@@ -1,8 +1,6 @@
 // JavaScript Document
-window.onresize = window.onload = function () {
-  var deviceWidth = document.documentElement.clientWidth; /*获取设备可视区宽*/
-  // if (deviceWidth > 750) deviceWidth = 750; /*最大不超过720，720为设计稿尺寸*/
-  document.documentElement.style.fontSize = deviceWidth / 10 + 'px';
-
-
+window.onresize = window.onload = function() {
+    var deviceWidth = document.documentElement.clientWidth; /*获取设备可视区宽*/
+    // if (deviceWidth > 750) deviceWidth = 750; /*最大不超过720，720为设计稿尺寸*/
+    document.documentElement.style.fontSize = deviceWidth / 10 + 'px';
 };
